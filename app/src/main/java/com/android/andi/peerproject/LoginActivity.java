7d080f706b90daabdaf4b8d3b0fbd2bcff22be4b
yizhang61 <yzhang3250@gatech.edu>
2018-02-08 02:45:49 -0500

    Add weather condition display, chat online, login and register information

---
+++ app/src/main/java/com/android/andi/peerproject/LoginActivity.java
@@ -0,0 +1,276 @@
+package com.mas8803.flashchat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    // Constants:
+    final int REQUEST_CODE = 123;
+    final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    // App ID to use OpenWeather data
+    final String APP_ID = "00bed0d82b87d022cf255eff4fd447a2";
+    // Time between location updates (5000 milliseconds or 5 seconds)
+    final long MIN_TIME = 5000;
+    // Distance between location updates (1000m or 1km)
+    final float MIN_DISTANCE = 1000;
+
+    String LOCATION_PROVIDER = LocationManager.GPS_PROVIDER;
+
+
+    // Member Variables:
+    TextView mCityLabel;
+    ImageView mWeatherImage;
+    TextView mTemperatureLabel;
+
+    LocationManager mLocationManager;
+    LocationListener mLocationListener;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.login_email);
+        mPasswordView = (EditText) findViewById(R.id.login_password);
+
+        mCityLabel = (TextView) findViewById(R.id.locationTV);
+        mWeatherImage = (ImageView) findViewById(R.id.weatherSymbolIV);
+        mTemperatureLabel = (TextView) findViewById(R.id.tempTV);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.integer.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("FlashChat", "onResume() called");
+
+        Intent myIntent = getIntent();
+        String city = myIntent.getStringExtra("City");
+
+        if (city != null) {
+            getWeatherForNewCity(city);
+
+        } else {
+            Log.d("FlashChat", "Getting weather for current location");
+            getWeatherForCurrentLocation();
+        }
+    }
+
+
+
+    // Executed when Sign in button pressed
+    public void signInExistingUser(View v)   {
+        attemptLogin();
+
+    }
+
+    // Executed when Register button pressed
+    public void registerNewUser(View v) {
+        Intent intent = new Intent(this, com.mas8803.flashchat.RegisterActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+
+    private void attemptLogin() {
+
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        if (email.equals("") || password.equals("")) {
+                return;
+        }
+        Toast.makeText(this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("FlashChat", "signInWithEmail() onComplete" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.d("FlashChat", "Problem signing in: " + task.getException());
+                    showErrorDialog("There was a problem signing in");
+                } else {
+                    Intent intent = new Intent(LoginActivity.this, MainChatActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+    }
+
+    private void getWeatherForCurrentLocation() {
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Clima", "onLocationChange() callback received");
+                String longitude = String.valueOf(location.getLongitude());
+                String latitude = String.valueOf(location.getLatitude());
+                RequestParams params = new RequestParams();
+                params.put("lat", latitude);
+                params.put("lon", longitude);
+                params.put("appid", APP_ID);
+                letsDoSomeNetworking(params);
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("FlashChat", "onLocationChange() callback received");
+            }
+
+        };
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this,
+                    new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LOCATION_PROVIDER, MIN_TIME, MIN_DISTANCE, mLocationListener);
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("FlashChat", "onRequestPermissionsResult granted");
+            } else {
+                Log.d("FlashChat", "onRequestPermissionsResult denied");
+            }
+        }
+    }
+
+
+    private void getWeatherForNewCity(String city) {
+        RequestParams params = new RequestParams();
+        params.put("q", city);
+        params.put("appid", APP_ID);
+        letsDoSomeNetworking(params);
+    }
+
+
+    private void letsDoSomeNetworking(RequestParams params) {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess (int statusCode, Header[] headers, JSONObject response) {
+                Log.d("FlashChat", "Success! JSON: "+ response.toString());
+                WeatherDataModel weatherData = WeatherDataModel.fromJason(response);
+                updateUI(weatherData);
+            }
+
+            @Override
+            public void onFailure (int statusCode, Header[] headers, Throwable e, JSONObject response) {
+                Log.e("FlashChat", "Fail" + e.toString());
+                Log.d("FlashChat", "Status code " + statusCode);
+                Toast.makeText(LoginActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+    }
+
+    private void updateUI (WeatherDataModel weather) {
+        mTemperatureLabel.setText(weather.getTemperature());
+        mCityLabel.setText(weather.getCity());
+        int resourceID = getResources().getIdentifier(weather.getIconName(), "drawable", getPackageName());
+        mWeatherImage.setImageResource(resourceID);
+    }
+
+
+
+    private void showErrorDialog(String message) {
+        new AlertDialog.Builder(this)
+                .setTitle("Oops")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mLocationManager != null) {
+            mLocationManager.removeUpdates(mLocationListener);
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/android/andi/peerproject/MainChatActivity.java
@@ -0,0 +1,137 @@
+package com.mas8803.flashchat;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+
+public class MainChatActivity extends AppCompatActivity {
+
+
+    private String mDisplayName;
+    private ListView mChatListView;
+    private EditText mInputText;
+    private ImageButton mSendButton;
+    private DatabaseReference mDatabaseReference;
+    private ChatListAdapter mAdapter;
+    private ListView function_list;
+    private MyAdapter myAdapter=null;
+    private List<String> functions=null;
+    private Context mContext=null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chat);
+
+        setupDisplayName();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // Link the Views in the layout to the Java code
+        mInputText = (EditText) findViewById(R.id.messageInput);
+        mSendButton = (ImageButton) findViewById(R.id.sendButton);
+        mChatListView = (ListView) findViewById(R.id.chat_list_view);
+
+//        functions=new LinkedList<>();
+//        functions.add("Calendar API");
+//        functions.add("Notification");
+//        myAdapter=new MyAdapter((LinkedList<String>) functions, mContext);
+//        function_list.setAdapter(myAdapter);
+
+        mInputText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                sendMessage();
+                return true;
+            }
+        });
+
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
+        });
+
+
+//        mContext=MainChatActivity.this;
+//        bindViews();
+//        function_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                Intent intent = new Intent(MainChatActivity.this, demos[position].demoClass);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+//
+//    private void bindViews(){
+//        function_list = (ListView) findViewById(R.id.list_function);
+//    }
+
+
+
+    private void setupDisplayName() {
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+        if (mDisplayName == null) {
+            mDisplayName = "Anonymous";
+        }
+    }
+
+    private void sendMessage() {
+
+        Log.d("FlashChat", "I do something");
+
+        String input = mInputText.getText().toString();
+        if (!input.equals("")) {
+            InstantMessage chat = new InstantMessage(input, mDisplayName);
+            mDatabaseReference.child("messages").push().setValue(chat);
+            mInputText.setText("");
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAdapter = new ChatListAdapter(this, mDatabaseReference, mDisplayName);
+        mChatListView.setAdapter(mAdapter);
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAdapter.cleanup();
+    }
+
+    private static class DemoInfo {
+        private final Class<? extends android.app.Activity> demoClass;
+
+        public DemoInfo(Class<? extends android.app.Activity> demoClass) {
+            this.demoClass = demoClass;
+        }
+    }
+
+    private static final DemoInfo[] demos = {
+            new DemoInfo(Calendar.class),
+            new DemoInfo(Notification.class)
+    };
+
+}
